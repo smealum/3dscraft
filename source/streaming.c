58863907aadb26b6257eb5d9e6e9d805ee16d227
smea <smealum@gmail.com>
2014-11-06 18:37:20 -0800

    added a bunch of streaming code; in theory should let us load/save data, but #untested #githashtag

--- source/streaming.c
+++ source/streaming.c
@@ -4,6 +4,8 @@
 #include "streaming.h"
 #include "configuration.h"
 
+#define CHUNK_DATASIZE (CLUSTER_SIZE*CLUSTER_SIZE*CLUSTER_SIZE*CHUNK_HEIGHT)
+
 void initChunkRepository(chunkRepository_s* cr)
 {
 	if(!cr)return;
@@ -25,30 +27,17 @@ void addChunkToRepository(chunkRepository_s* cr, s32 x, s32 z, u32 offset)
 	cr->first=ci;
 }
 
-bool isChunkInRepository(chunkRepository_s* cr, s32 x, s32 z)
+chunkInfo_s* getChunkFromRepository(chunkRepository_s* cr, s32 x, s32 z)
 {
-	if(!cr)return false;
+	if(!cr)return NULL;
 
 	chunkInfo_s* ci=cr->first;
 	while(ci)
 	{
-		if(ci->position.x==x && ci->position.z==z)return true;
+		if(ci->position.x==x && ci->position.z==z)return ci;
 		ci=ci->next;
 	}
-	return false;
-}
-
-u32 getChunkOffset(chunkRepository_s* cr, s32 x, s32 z)
-{
-	if(!cr)return 0;
-
-	chunkInfo_s* ci=cr->first;
-	while(ci)
-	{
-		if(ci->position.x==x && ci->position.z==z)return ci->offset;
-		ci=ci->next;
-	}
-	return 0;
+	return NULL;
 }
 
 int readWorldStreamHeader(worldStream_s* ws)
@@ -67,7 +56,7 @@ int readWorldStream(worldStream_s* ws)
 	if(!ws)return -1;
 
 	static fileListHeader_s buffer;
-	u32 offset=sizeof(fileWorldHeader_s);
+	u32 offset=ws->header.first;
 	u32 bytesRead=0;
 	do{
 		Result ret=FSFILE_Read(ws->file, &bytesRead, offset, &buffer, sizeof(fileListHeader_s));
@@ -79,6 +68,110 @@ int readWorldStream(worldStream_s* ws)
 	return 0;
 }
 
+void createFileList(fileListHeader_s* flh)
+{
+	if(!flh)return;
+
+	flh->length=0;
+	flh->next=0;
+}
+
+int addChunkToList(fileListHeader_s* flh, s32 x, s32 z, u32 offset)
+{
+	if(!flh)return -1;
+	if(flh->length>=FILELISTBLOCK_LENGTH)return -2; //no room left !
+
+	flh->list[flh->length].x=x;
+	flh->list[flh->length].z=z;
+	flh->list[flh->length].offset=offset;
+
+	flh->length++;
+
+	return 0;
+}
+
+void flushWorldStream(worldStream_s* ws)
+{
+	if(!ws || ws->flushed)return;
+	u32 bytesWritten=0;
+	Result ret;
+
+	ret=FSFILE_Write(ws->file, &bytesWritten, 0, &ws->header, sizeof(fileWorldHeader_s), FS_WRITE_FLUSH);
+	if(ret || bytesWritten!=sizeof(fileWorldHeader_s))return;
+
+	ret=FSFILE_Write(ws->file, &bytesWritten, ws->header.last, &ws->current, sizeof(fileListHeader_s), FS_WRITE_FLUSH);
+	if(ret || bytesWritten!=sizeof(fileListHeader_s))return;
+
+	ws->flushed=true;
+}
+
+void addChunkToWorldStream(worldStream_s* ws, s32 x, s32 z, u8* data)
+{
+	if(!ws || !data)return;
+	u32 bytesWritten=0;
+	u32 offset=ws->size;
+
+	Result ret=FSFILE_Write(ws->file, &bytesWritten, offset, data, CHUNK_DATASIZE, FS_WRITE_FLUSH);
+	if(ret || bytesWritten!=CHUNK_DATASIZE)return;
+	ws->size+=bytesWritten;
+
+	ret=addChunkToList(&ws->current, x, z, offset);
+	if(ret && ret!=-2)return;
+	if(ret==-2)
+	{
+		flushWorldStream(ws);
+		createFileList(&ws->current);
+		addChunkToList(&ws->current, x, z, offset);
+		ws->header.last=ws->size;
+		ws->size+=sizeof(fileListHeader_s);
+	}
+
+	ws->flushed=false;
+}
+
+void saveChunk(worldStream_s* ws, s32 x, s32 z, u8* data)
+{
+	if(!ws || !data)return;
+
+	chunkInfo_s* ci=getChunkFromRepository(&ws->chunks, x, z);
+	if(ci)
+	{
+		//overwrite chunk that already exists
+		u32 bytesWritten=0;
+		Result ret=FSFILE_Write(ws->file, &bytesWritten, ci->offset, data, CHUNK_DATASIZE, FS_WRITE_FLUSH);
+		if(ret || bytesWritten!=CHUNK_DATASIZE)return;
+	}else{
+		//add new chunk to file
+		addChunkToWorldStream(ws, x, z, data);
+	}
+}
+
+int createWorldStream(worldStream_s* ws)
+{
+	if(!ws)return -1;
+	Result ret;
+
+	ret=FSUSER_OpenFile(NULL, &ws->file, configuration.sdmc, FS_makePath(PATH_CHAR, ws->path), FS_OPEN_READ|FS_OPEN_WRITE|FS_OPEN_CREATE, FS_ATTRIBUTE_NONE);
+	if(ret){svcCloseHandle(ws->file); return -2;}
+
+	u32 bytesWritten=0;
+
+	ws->header.magic=WORLDFILE_MAGIC;
+	ws->header.first=ws->header.last=sizeof(fileWorldHeader_s);
+
+	ws->size=0;
+	ret=FSFILE_Write(ws->file, &bytesWritten, ws->size, &ws->header, sizeof(fileWorldHeader_s), FS_WRITE_FLUSH);
+	if(ret || bytesWritten!=sizeof(fileWorldHeader_s))return -1;
+	ws->size+=bytesWritten;
+
+	createFileList(&ws->current);
+	ret=FSFILE_Write(ws->file, &bytesWritten, ws->size, &ws->current, sizeof(fileListHeader_s), FS_WRITE_FLUSH);
+	if(ret || bytesWritten!=sizeof(fileListHeader_s))return -1;
+	ws->size+=bytesWritten;
+
+	return 0;
+}
+
 void initWorldStream(worldStream_s* ws, char* path)
 {
 	if(!ws || !path)return;
@@ -86,23 +179,28 @@ void initWorldStream(worldStream_s* ws, char* path)
 	initChunkRepository(&ws->chunks);
 	strncpy(ws->path, path, STREAMPATH_LENGTH);
 
-	Result ret=FSUSER_OpenFile(NULL, &ws->file, configuration.sdmc, FS_makePath(PATH_CHAR, ws->path), FS_OPEN_READ, FS_ATTRIBUTE_NONE);
-	if(ret){svcCloseHandle(ws->file); return;}
-
-	readWorldStreamHeader(ws);
-	readWorldStream(ws);
+	Result ret=FSUSER_OpenFile(NULL, &ws->file, configuration.sdmc, FS_makePath(PATH_CHAR, ws->path), FS_OPEN_READ|FS_OPEN_WRITE, FS_ATTRIBUTE_NONE);
+	if(ret)
+	{
+		svcCloseHandle(ws->file);
+		if(createWorldStream(ws)){svcCloseHandle(ws->file); return;}
+	}else{
+		readWorldStreamHeader(ws);
+		readWorldStream(ws);
+	}
+	ws->flushed=true;
 }
 
 int getChunkFromStream(worldStream_s* ws, s32 x, s32 z, u8* dst)
 {
 	if(!ws || !dst)return -1;
 	
-	u32 offset=getChunkOffset(&ws->chunks, x, z);
-	if(offset)
+	chunkInfo_s* ci=getChunkFromRepository(&ws->chunks, x, z);
+	if(ci)
 	{
 		u32 bytesRead=0;
-		u32 size=CLUSTER_SIZE*CLUSTER_SIZE*CLUSTER_SIZE*CHUNK_HEIGHT;
-		Result ret=FSFILE_Read(ws->file, &bytesRead, offset, dst, size);
+		u32 size=CHUNK_DATASIZE;
+		Result ret=FSFILE_Read(ws->file, &bytesRead, ci->offset, dst, size);
 		if(ret || bytesRead!=size)return -2;
 		return 0;
 	}
@@ -114,7 +212,7 @@ bool isChunkInStream(worldStream_s* ws, s32 x, s32 z)
 {
 	if(!ws)return false;
 
-	return isChunkInRepository(&ws->chunks, x, z);
+	return getChunkFromRepository(&ws->chunks, x, z)!=NULL;
 }
 
 void exitWorldStream(worldStream_s* ws)
--- source/streaming.h
+++ source/streaming.h
@@ -5,11 +5,15 @@
 
 #include "math.h"
 
+#define WORLDFILE_MAGIC (0x43534433)
 #define STREAMPATH_LENGTH (256)
 #define FILELISTBLOCK_LENGTH (64)
 
 typedef struct
 {
+	u32 magic; //MAGIC
+	u32 first; //offset to first list
+	u32 last; //offset to last list
 	u32 reserved[16];
 }fileWorldHeader_s;
 
@@ -19,6 +23,7 @@ typedef struct
 		s32 x, z;
 		u32 offset;
 	} list[FILELISTBLOCK_LENGTH];
+	u16 length; //list length
 	u32 next; //file offset
 	u32 reserved[8];
 }fileListHeader_s;
@@ -41,13 +46,17 @@ typedef struct
 {
 	fileWorldHeader_s header;
 	chunkRepository_s chunks;
+	fileListHeader_s current;
 	char path[STREAMPATH_LENGTH];
 	Handle file;
+	u32 size;
+	bool flushed;
 }worldStream_s;
 
 void initWorldStream(worldStream_s* ws, char* path);
 bool isChunkInStream(worldStream_s* ws, s32 x, s32 z);
 int getChunkFromStream(worldStream_s* ws, s32 x, s32 z, u8* dst);
+void saveChunk(worldStream_s* ws, s32 x, s32 z, u8* data);
 void exitWorldStream(worldStream_s* ws);
 
 #endif
