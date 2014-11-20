9351e2fd12fe25c45a5fa80e62c3419ed7547d8a
smea <smealum@gmail.com>
2014-11-19 20:06:32 -0800

    fixed argv handling

--- source/configuration.c
+++ source/configuration.c
@@ -11,18 +11,19 @@ static const FS_archive sdmcArchive={0x00000009, (FS_path){PATH_EMPTY, 1, (u8*)"
 void initConfiguration(configuration_s* c, int argc, char** argv)
 {
 	if(!c)c=&configuration;
-	if(argc && argv)
+	if(argc && argv && !memcmp("sdmc:", argv[0], 5))
 	{
 		//grab path from arguments
 		int l;
 		for(l=strlen(argv[0]); l>=0 && argv[0][l]!='/'; l--);
+		l-=5;
 		c->path=malloc(l+1);
-		memcpy(c->path, argv[0], l);
+		memcpy(c->path, &argv[0][5], l);
 		c->path[l+1]=0x00;
 	}else{
 		c->path=(char*)defaultPath;
 	}
-
+	print("%s\n%s\n",argv[0],c->path);
 	c->sdmc=sdmcArchive;
 	Result ret=FSUSER_OpenArchive(NULL, &c->sdmc);
 	if(ret)print("couldn't open SDMC : %08X\n",(unsigned int)ret);
